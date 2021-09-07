b0813deb6efa6cecd373580ca1a6d97e2c62a6c6
Dimitar Stoev <dimitarg.stoev@gmail.com>
2021-09-07 23:20:50 +0300

    navigation menu

--- components/LayoutComponent/Layout.tsx
+++ components/LayoutComponent/Layout.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Navigation from "../NavigationComponent/Navigation";
+import styles from './layout.module.css';
 
 const Layout = ({ children }: any) => {
   return (
     <React.Fragment>
       <Navigation />
-      <main>{children}</main>
+      <main className={styles.layout}>{children}</main>
     </React.Fragment>
   );
 };
--- components/NavigationComponent/Navigation.tsx
+++ components/NavigationComponent/Navigation.tsx
@@ -1,75 +1,95 @@
 import Link from "next/link";
 import styles from "./navigation.module.css";
 import React, { useState, useEffect } from "react";
-import Hamburger from 'hamburger-react'
+import Hamburger from 'hamburger-react';
+import Image from 'next/image';
+import image from '../../public/assets/logo.png';
+
 
 const Navigation = () => {
-  const [isNavSidebarActive, setIsNavSidebarAcive] = useState<Boolean>(false);
+  const [isNavSidebarActive, setIsNavSidebarAcive] = useState<boolean>(false);
   const [currentRoute, setCurrentRoute] = useState<String>();
+  const [isRouteActive, setIsRouteActive] = useState([false, false, false, false, false, false]);
 
   const handleClick = () => {
     setIsNavSidebarAcive(!isNavSidebarActive);
   };
 
-  const handleRouteChange = (word: String) => {
+  const handleRouteChange = (word: String, indexMenu: number) => {
     setCurrentRoute(word);
     setIsNavSidebarAcive(!isNavSidebarActive);
+    let replacingArray = [false, false, false, false, false, false]
+    replacingArray[indexMenu] = true;
+    setIsRouteActive(replacingArray)
   };
 
   useEffect(() => {
     setCurrentRoute(window.location.pathname.replace(/\//, ""));
+    if (currentRoute === '' || currentRoute === undefined) {
+      setCurrentRoute('Home');
+      let replacingArray = [false, false, false, false, false, false]
+      replacingArray[0] = true;
+      setIsRouteActive(replacingArray)
+    }
   }, []);
 
-  return isNavSidebarActive ? (
+  return (
     <React.Fragment>
       <ul className={styles.container}>
-        <li>{currentRoute}</li>
         <li>
-          <button onClick={handleClick} className={styles.button}>
-            X
-          </button>
+          <Link href="/">
+            <Image src={image} className={styles.imageControl} alt="Contact logo page" />
+          </Link>
+        </li>
+        <li className={styles.liControl}>
+          <li>
+            <h4>{currentRoute}</h4>
+          </li>
+          <li>
+            <Hamburger toggled={isNavSidebarActive} toggle={handleClick} />
+          </li>
         </li>
       </ul>
-      <nav className={styles.nav}>
+      <nav className={isNavSidebarActive ? styles.nav : styles.hideMenu}>
         <ul>
           <li>
+            <div className={isRouteActive[0] ? styles.routeActive : ''}></div>
             <Link href="/">
-              <a onClick={() => handleRouteChange("Home")}>Home</a>
+              <a onClick={() => handleRouteChange("Home", 0)}>Home</a>
             </Link>
           </li>
           <li>
+            <div className={isRouteActive[1] ? styles.routeActive : ''}></div>
             <Link href="/industries">
-              <a onClick={() => handleRouteChange("Industries")}>Industries</a>
+              <a onClick={() => handleRouteChange("Industries", 1)}>Industries</a>
             </Link>
           </li>
           <li>
+            <div className={isRouteActive[2] ? styles.routeActive : ''}></div>
             <Link href="/news">
-              <a onClick={() => handleRouteChange("News")}>News</a>
+              <a onClick={() => handleRouteChange("News", 2)}>News</a>
             </Link>
           </li>
           <li>
+            <div className={isRouteActive[3] ? styles.routeActive : ''}></div>
             <Link href="/about">
-              <a onClick={() => handleRouteChange("About")}>About</a>
+              <a onClick={() => handleRouteChange("About", 3)}>About</a>
             </Link>
           </li>
           <li>
+            <div className={isRouteActive[4] ? styles.routeActive : ''}></div>
             <Link href="/career">
-              <a onClick={() => handleRouteChange("Career")}>Career</a>
+              <a onClick={() => handleRouteChange("Career", 4)}>Career</a>
             </Link>
           </li>
           <li>
+            <div className={isRouteActive[5] ? styles.routeActive : ''}></div>
             <Link href="/contact">
-              <a onClick={() => handleRouteChange("Contact")}>Contact</a>
+              <a onClick={() => handleRouteChange("Contact", 5)}>Contact</a>
             </Link>
           </li>
         </ul>
       </nav>
-    </React.Fragment>
-  ) : (
-    <React.Fragment>
-      <div className={styles.burger}>
-        <Hamburger toggled={isNavSidebarActive} toggle={handleClick} />
-      </div>
     </React.Fragment>
   );
 };
