f918d94729f88b28369d92d908de76c8311b8063
DrowSa <drowsa@pop-os.localdomain>
2021-10-11 23:41:26 +0300

    career design screen

---
+++ components/careerForm/CareerForm.tsx
@@ -0,0 +1,230 @@
+import React, { useState } from "react";
+import styles from "./careerForm.module.css";
+
+const CareerForm = (): JSX.Element => {
+  const [titleIndex, setTitleIndex] = useState<number>(0);
+  const [expIndex, setExpIndex] = useState<number>(0);
+  const [researchSlider, setResearchSlider] = useState<number>(75);
+  const [userJourneySlider, setUserJourner] = useState<number>(50);
+  const [wireFramingSlider, setWireFraming] = useState<number>(75);
+  const [highFidelitySlider, setHighFidelity] = useState<number>(95);
+  const [prototypingSlider, setPrototyping] = useState<number>(60);
+
+  const handleTitleIndex = (index: number) => {
+    setTitleIndex(index);
+  };
+
+  const handleExpIndex = (index: number) => {
+    setExpIndex(index);
+  };
+
+  const handleSliderResearch = (event: any) => {
+    if (event && event !== undefined) {
+      setResearchSlider(event.target.value);
+    }
+  };
+
+  const handleSliderUserJourney = (event: any) => {
+    if(event && event !== undefined) {
+      setUserJourner(event.target.value);
+    }
+  }
+
+  const handleSliderWireframing = (event: any) => {
+    if(event && event !== undefined) {
+      setWireFraming(event.target.value);
+    }
+  }
+
+  const handleSliderHighFidelity = (event: any) => {
+    if(event && event !== undefined) {
+      setHighFidelity(event.target.value);
+    }
+  }
+
+  const handleSliderPrototyping = (event: any) => {
+    if(event && event !== undefined) {
+      setPrototyping(event.target.value);
+    }
+  }
+
+  return (
+    <div>
+      <div className={styles.sack}>
+        <div
+          onClick={() => handleTitleIndex(1)}
+          className={
+            titleIndex === 1 ? styles.jobBallBlack : styles.jobBallWhite
+          }
+        >
+          <h2>Software</h2>
+          <h2>Engineer</h2>
+        </div>
+        <div
+          onClick={() => handleTitleIndex(2)}
+          className={
+            titleIndex === 2 ? styles.jobBallBlack : styles.jobBallWhite
+          }
+        >
+          <h2>Designer</h2>
+        </div>
+        <div
+          onClick={() => handleTitleIndex(3)}
+          className={
+            titleIndex === 3 ? styles.jobBallBlack : styles.jobBallWhite
+          }
+        >
+          <h2>Other</h2>
+        </div>
+      </div>
+      <h2>Experience</h2>
+      <div className={styles.line}></div>
+      <div className={styles.exp}>
+        <div className={styles.expDIV}>
+          <span
+            onClick={() => handleExpIndex(1)}
+            className={expIndex === 1 ? styles.blackSpan : ""}
+          >
+            &lt; 1 year
+          </span>
+          <span
+            onClick={() => handleExpIndex(2)}
+            className={expIndex === 2 ? styles.blackSpan : ""}
+          >
+            1-3 year
+          </span>
+        </div>
+        <div className={styles.expDIV}>
+          <span
+            onClick={() => handleExpIndex(3)}
+            className={expIndex === 3 ? styles.blackSpan : ""}
+          >
+            3-5 years
+          </span>
+          <span
+            onClick={() => handleExpIndex(4)}
+            className={expIndex === 4 ? styles.blackSpan : ""}
+          >
+            More than 5 years
+          </span>
+        </div>
+      </div>
+      <h2>I am good at</h2>
+      <div className={styles.line}></div>
+      <div className={styles.radioButtons}>
+        <div>
+          <input
+            id="mobile"
+            value="mobileApplications"
+            name="platform"
+            type="radio"
+          />
+          <label htmlFor="Mobile Applications">Mobile Applications</label>
+        </div>
+        <div>
+          <input
+            id="ar"
+            value="augmentedReality"
+            name="platform"
+            type="radio"
+          />
+          <label htmlFor="Augmented reality">Augmented reality</label>
+        </div>
+        <div>
+          <input
+            id="shopsAndWeb"
+            value="shopsAndWeb"
+            name="platform"
+            type="radio"
+          />
+          <label htmlFor="Online shops&Web">Online shops&Web</label>
+        </div>
+        <div>
+          <input id="other" value="other" name="platform" type="radio" />
+          <label htmlFor="and many more">and many more</label>
+        </div>
+      </div>
+      <h2>Skills</h2>
+      <div className={styles.line}></div>
+      <div className={styles.slidersBox}>
+        <div className={styles.flexRow}>
+          <h4>Research</h4>
+          <input
+            onChange={() => handleSliderResearch(event)}
+            type="range"
+            min="1"
+            max="100"
+            value={researchSlider}
+            className={styles.slider}
+          ></input>
+        </div>
+        <div className={styles.flexRow}>
+        <h4>User journey</h4>
+          <input
+            onChange={() => handleSliderUserJourney(event)}
+            type="range"
+            min="1"
+            max="100"
+            value={userJourneySlider}
+            className={styles.slider}
+          ></input>
+        </div>
+        <div className={styles.flexRow}>
+        <h4>Wireframing</h4>
+          <input
+            onChange={() => handleSliderWireframing(event)}
+            type="range"
+            min="1"
+            max="100"
+            value={wireFramingSlider}
+            className={styles.slider}
+          ></input>
+        </div>
+        <div className={styles.flexRow}>
+        <h4>High Fidelity</h4>
+          <input
+            onChange={() => handleSliderHighFidelity(event)}
+            type="range"
+            min="1"
+            max="100"
+            value={highFidelitySlider}
+            className={styles.slider}
+          ></input>
+        </div>
+        <div className={styles.flexRow}>
+        <h4>Prototyping</h4>
+          <input
+            onChange={() => handleSliderPrototyping(event)}
+            type="range"
+            min="1"
+            max="100"
+            value={prototypingSlider}
+            className={styles.slider}
+          ></input>
+        </div>
+      </div>
+      <h2>Id love to show you</h2>
+      <div className={styles.line}></div>
+      <p>portfolion, dreams, plans? Webe missed something? Just shoot!</p>
+      <div className={styles.textAreaWidthControl}>
+      <textarea
+      className={styles.textAreaControl}
+        name="otherInformation"
+        id="textAreaControl"
+        cols={30}
+        rows={5}
+        placeholder="3,2,1 Go!"
+      ></textarea>
+      </div>
+      <h2>A little bit of paperwork</h2>
+      <h2>and we are all set:</h2>
+      <div className={styles.line}></div>
+      <input type="text" placeholder="Your name" />
+      <input type="text" placeholder="Your company name" />
+      <input type="text" placeholder="Your email" />
+      <input type="text" placeholder="Your phone number" />
+    </div>
+  );
+};
+
+export default CareerForm;
--- pages/career/index.tsx
+++ pages/career/index.tsx
@@ -3,10 +3,23 @@ import React from "react";
 import styles from "../../styles/career.module.css";
 import Texts from "./jobs.json";
 import MultiBall from "../../components/MultipleBalls/MultiBall";
+import Image from "next/image";
+import image from "../../public/assets/career/careerBanner.webp";
+import CareerForm from "../../components/careerForm/CareerForm";
 
 const Career = () => {
   return (
     <div>
+      <div className={styles.logoPage}>
+        <div className={styles.body}>
+        <Image
+          src={image}
+          className={styles.imageControl}
+          alt="Career logo page"
+        />
+        </div>
+        <h1>Career</h1>
+      </div>
       <div className={styles.container}>
         <h1>Bring your</h1>
         <h2>talent, hard work and</h2>
@@ -14,11 +27,25 @@ const Career = () => {
         <h2>make it work for you</h2>
         <div className={styles.line}></div>
         <div className={styles.flexRow}>
-          <div></div>
+          <div className={styles.flexColumn}>
+            <div className={styles.flexRow}>
+              <div className={styles.circle}></div>
+              <div className={styles.circle}></div>
+            </div>
+            <div className={styles.flexRow}>
+              <div className={styles.circle}></div>
+              <div className={styles.circle}></div>
+            </div>
+            <div className={styles.flexRow}>
+              <div className={styles.circle}></div>
+              <div className={styles.circle}></div>
+            </div>
+          </div>
           <div>
             <p>
-              Below you can see the open positions. If you don&apos;t see what you
-              are looking for, scroll down, we&apos;ve got something for you.
+              Below you can see the open positions. If you don&apos;t see what
+              you are looking for, scroll down, we&apos;ve got something for
+              you.
             </p>
           </div>
         </div>
@@ -26,13 +53,15 @@ const Career = () => {
       <div className={styles.openPositions}>
         {Texts.map((item) => {
           return (
-              <div key={item.title} className={styles.position}>
-                <div>
-                  <h1>{item.title}</h1>
-                  <p>{item.description}</p>
-                </div>
-                <button onClick={() => router.push('/career/' + item.title)}><div className={styles.arrowRight}></div></button>
+            <div key={item.title} className={styles.position}>
+              <div>
+                <h1>{item.title}</h1>
+                <p>{item.description}</p>
               </div>
+              <button onClick={() => router.push("/career/" + item.title)}>
+                <div className={styles.arrowRight}></div>
+              </button>
+            </div>
           );
         })}
       </div>
@@ -40,9 +69,13 @@ const Career = () => {
         <h2>Because we are a team and we always try to find the best fit...</h2>
         <div className={styles.line}></div>
         <MultiBall />
-        <p>...if you think you are a match, tell us a little bit more about yourself:</p>
+        <p>
+          ...if you think you are a match, tell us a little bit more about
+          yourself:
+        </p>
         <h2>Lets start! You are?</h2>
         <div className={styles.line}></div>
+        <CareerForm />
       </div>
     </div>
   );
